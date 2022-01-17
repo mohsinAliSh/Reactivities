e802f6f0c73d267b3e79304891c6d5fb0789d207
mohsinAliSh <kustmohsin@gmail.com>
2022-01-17 12:17:11 +0500

    End of Section 5

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -8,7 +8,7 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class BaseApiController:ControllerBase
     {
-        private IMediator _mediator;
+        private IMediator? _mediator;
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './app/layout/styles.css';
 // import 'fomantic-ui-css/semantic.css';
 //import 'fomantic-ui-css/semantic.css';
-import App from './App';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -11,7 +11,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuritng performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
