310f42089a5689ba31a4f39cfbf559dad99363fd
mohsinAliSh <kustmohsin@gmail.com>
2022-01-24 16:52:30 +0500

    End of Section 10

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,8 @@
 
 using API.Extensions;
+using API.Middleware;
+using Application.Activities;
+using FluentValidation.AspNetCore;
 
 
 namespace API
@@ -15,18 +18,19 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(config=>{
+                config.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
             services.AddAppplicationServices(_config);
-           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
 
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
 
