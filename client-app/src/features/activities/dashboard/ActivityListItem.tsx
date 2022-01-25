a551f9521564047f47e2a9187cc5b4b7d9bd9a84
mohsinAliSh <kustmohsin@gmail.com>
2022-01-25 11:06:37 +0500

    End of Section 11

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,59 +1,60 @@
+import { format } from "date-fns";
 import React, { SyntheticEvent, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/store";
 
-interface Props{
-    activity:Activity
+interface Props {
+    activity: Activity
 }
 
 
-export default function ActivityListItem({activity}:Props){
-    
-        const {activityStore} = useStore();
-        const {deleteActivity, loading} = activityStore;
-    
-        const [target, setTarget] = useState('');
-    
-        function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-            setTarget(e.currentTarget.name);
-            deleteActivity(id);
-        }
-        return(
-            <Segment.Group>
-                <Segment>
-                    <Item.Group>
-                        <Item>
-                            <Item.Image size='tiny' circular src='/assets/user.png'/>
-                            <Item.Content>
-                                <Item.Header as={Link} to={`/activities/${activity.id}`}>
-                                    {activity.title}
-                                </Item.Header>
-                                <Item.Description>Hosted by Bob</Item.Description>
-                            </Item.Content>
-                        </Item>
-                    </Item.Group>
-                </Segment>
-                <Segment>
-                    <span>
-                        <Icon name='clock'/> {activity.date}
-                        <Icon name='marker'/> {activity.venue}
-                    </span>
-                </Segment>
-                <Segment secondary>
-                    Attendess go here
-                </Segment>
-                <Segment clearing>
-                    <span>{activity.description}</span>
-                    <Button
+export default function ActivityListItem({ activity }: Props) {
+
+    const { activityStore } = useStore();
+    const { deleteActivity, loading } = activityStore;
+
+    const [target, setTarget] = useState('');
+
+    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+        setTarget(e.currentTarget.name);
+        deleteActivity(id);
+    }
+    return (
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size='tiny' circular src='/assets/user.png' />
+                        <Item.Content>
+                            <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                                {activity.title}
+                            </Item.Header>
+                            <Item.Description>Hosted by Bob</Item.Description>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' /> {format (activity.date!,'dd MMM yyyy h:mm aa')}
+                    <Icon name='marker' /> {activity.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                Attendess go here
+            </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button
                     as={Link}
                     to={`/activities/${activity.id}`}
                     color='teal'
                     floated='right'
                     content='View'
-                    />
-                </Segment>
-            </Segment.Group>
-        )
+                />
+            </Segment>
+        </Segment.Group>
+    )
 }
