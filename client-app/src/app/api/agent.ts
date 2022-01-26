0df304b2110e4000dfc777af2c17153d75d94829
mohsinAliSh <kustmohsin@gmail.com>
2022-01-26 16:49:30 +0500

    End of Section 13

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { resolve } from "path/posix";
 import { toast } from "react-toastify";
 import { history } from "../..";
 import { Activity } from "../models/activity";
+import { User, UserFormValues } from "../models/user";
 import { store } from "../stores/store";
 
 const sleep=(delay:number)=>{
@@ -12,6 +13,11 @@ return new Promise((resolve)=>{
 }
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
+axios.interceptors.request.use(config=>{
+    const token=store.commonStore.token;
+    if(token) config.headers!.Authorization=`Bearer ${token}`
+    return config;
+})
 axios.interceptors.response.use(async response => {
 
         await sleep(1000);
@@ -67,7 +73,13 @@ const Activities = {
     update:(activity:Activity)=>axios.put(`/activities/${activity.id}`,activity),
     delete:(id:string)=>axios.delete<void>(`activities/${id}`)
 }
+const Account={
+    current:()=>requests.get<User>('/account'),
+    login:(user:UserFormValues) => requests.post<User>('/account/login',user),
+    register:(user:UserFormValues) => requests.post<User>('/account/register',user)
+}
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 export default agent;
