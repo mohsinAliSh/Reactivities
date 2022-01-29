0fdd7dc85c87971dc0b8a318fc29c57ae6d272dd
mohsinAliSh <kustmohsin@gmail.com>
2022-01-29 17:20:19 +0500

    End of section 17

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class ProfileController:BaseApiController
+    public class ProfilesController:BaseApiController
     {
         [HttpGet("{username}")]
         public async Task<IActionResult> GetProfile(string username){
