95cf7038dd530fa7945e28d4d550d9f15490d092
mohsinAliSh <kustmohsin@gmail.com>
2022-02-01 12:38:06 +0500

    Heroku Deploy

--- API/Startup.cs
+++ API/Startup.cs
@@ -42,10 +42,13 @@ namespace API
 
             }
 
-
+            
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseCors("CorsPolicy");
             app.UseAuthentication();
@@ -57,6 +60,7 @@ namespace API
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<ChatHub>("/chat");
+                endpoints.MapFallbackToController("Index","Fallback");
             });
         }
     }
